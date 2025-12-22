1b6448f977a758b3e2f74b873a246dc4db9f255c
Learning-howto-Code <jaketorohopkins@icloud.com>
2025-12-21 16:04:25 -0800

    got cnn script ready

--- cnn.py
+++ cnn.py
@@ -13,10 +13,10 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
 # Filepaths
-train_paths=[ "//Users/abrahamhopkins/Downloads/Jakes_Model/if_water/train", "/Users/abrahamhopkins/Downloads/Jakes_Model/if_water/prod_train"] #trying adding prod data to train
+train_paths=[ "/Users/abrahamhopkins/Downloads/Jakes_Model/if_water/pi_data/train"] #trying adding prod data to train
 
-val_path= "/Users/abrahamhopkins/Downloads/Jakes_Model/if_water/val"
-test_path="/Users/abrahamhopkins/Downloads/Jakes_Model/if_water/test"
+val_path= "/Users/abrahamhopkins/Downloads/Jakes_Model/if_water/pi_data/val"
+# test_path="/Users/abrahamhopkins/Downloads/Jakes_Model/if_water/test"
 
 
 datagen= ImageDataGenerator(rescale=1./255)
@@ -52,14 +52,14 @@ class_mode=class_mode,
 color_mode='grayscale',
 seed=42
 )
-test_data = datagen.flow_from_directory(
-    test_path,
-    batch_size=batch_size,
-    target_size=image_size,
-    class_mode=class_mode,
-    color_mode='grayscale',
-    seed=42
-)
+# test_data = datagen.flow_from_directory(
+#     test_path,
+#     batch_size=batch_size,
+#     target_size=image_size,
+#     class_mode=class_mode,
+#     color_mode='grayscale',
+#     seed=42
+# )
 data_augmentation = tf.keras.Sequential([
 layers.RandomZoom(0.25),
 layers.RandomTranslation(0, 0.4),
@@ -98,6 +98,6 @@ min_delta=0.01,
 )
 
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-model.save(f"model_with_prod_lots_of_aug{timestamp}.keras")
+model.save(f"pi_data_model{timestamp}.keras")
 # test_loss, test_acc = model.evaluate(test_data)
 # print("Test accuracy:", test_acc)
