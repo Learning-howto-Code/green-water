30cdab912d7949374e44029a40e35c9e001f68e2
Learning-howto-Code <jaketorohopkins@icloud.com>
2026-02-16 13:00:17 -0800

    added code to find diffs for all iamges

--- model_eval.py
+++ model_eval.py
@@ -5,11 +5,14 @@ from keras.layers import *
 from keras.models import *
 from datetime import datetime
 from keras.preprocessing import image
-from sklearn.metrics import classification_report, classification_report, confusion_matrix
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, precision_recall_curve
 import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import json
+import os
+import cv2 as cv
+import numpy as np
 
 def plot(history, timestamp):
     acc = history.history.get('accuracy', [])
@@ -40,6 +43,7 @@ def plot(history, timestamp):
 
 # Confusion matrix on test set
 def matrix(model, test_data):
+
     # Extract true labels and predictions from ImageDataGenerator
     y_true = []
     y_pred_probs = []
@@ -65,3 +69,68 @@ def matrix(model, test_data):
     # Calculate accuracy
     accuracy = (cm[0][0] + cm[1][1]) / cm.sum()
     print(f"\nAccuracy: {accuracy:.4f}")
+
+def precision_recall(model, test_data):
+    title="Precision-Recall Curve"
+    y_true = []
+    y_pred_probs = []
+
+    for images, labels in test_data:
+        y_true.extend(labels)
+        predictions = model.predict(images, verbose=0)
+        y_pred_probs.extend(predictions)
+
+    y_true = np.array(y_true)
+    y_pred_probs = np.array(y_pred_probs).flatten()
+
+    precision, recall, _ = precision_recall_curve(y_true, y_pred_probs)
+
+    plt.figure(figsize=(6, 5))
+    plt.plot(recall, precision, label="PR curve")
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    plt.title(title)
+    plt.grid(True, linestyle="--", alpha=0.4)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+    plt.close()
+def diffs():
+    global new_filepath, diff
+    old_data = []
+    if os.path.exists("delta.json"):
+        with open("delta.json", "r") as f:
+            try:
+                loaded = json.load(f)
+                old_data = loaded if isinstance(loaded, list) else [] # test case for empty file
+            except json.JSONDecodeError:
+                old_data = []
+    for dirpath in os.walk("/Users/jakehopkins/Downloads/if_water/food_clean"):
+        dir = dirpath[0]
+        print(dir)
+        files = [f for f in os.listdir(dir) if f.endswith('.jpg')]
+        old_filepath = "/Users/jakehopkins/Downloads/if_water/food_clean/test/clean/img_20260125_113927_2.jpg"
+        for i in range(len(files)):
+            files = [f for f in os.listdir(dir) if f.endswith('.jpg')]
+            files = sorted(files)  # sorts the files by timetamp TIMESTAMPS SKIP A FEW NUMBERS
+            new_filepath = os.path.join(dir, files[i])
+
+            
+            old = cv.imread(old_filepath)
+            
+            new = cv.imread(new_filepath)
+        
+            diff = cv.absdiff(old, new)
+            diff = np.average(diff)
+
+            old_filepath = new_filepath
+            print(f"new filepath {new_filepath}")
+            print(f"old filepath {old_filepath}")
+            print(f"loop number {i}")
+            print(f"difference is {diff} out 255")            
+            old_data.append({
+                "filepath": new_filepath,
+                "diff": float(diff)
+            })
+    with open("delta.json", "w") as f:
+        json.dump(old_data, f, indent=2)    
--- test.py
+++ test.py
@@ -1,26 +1,50 @@
 import cv2 as cv
 import numpy as np
 import os
+import json
+
+with open("delta.json") as f:
+    data = json.load(f)
+    data = {item["filepath"]: item["diff"] for item in data}
+    print(data["/Users/jakehopkins/Downloads/if_water/food_clean/train/food/00662_img_20260124_130741_659.jpg"])
 
 def diffs():
-    dir = "/Users/jakehopkins/Downloads/if_water/food_clean/train/clean"
-    files = [f for f in os.listdir(dir) if f.endswith('.jpg')] # so it doesn't get thrown off w
-    old_filepath = "/Users/jakehopkins/Downloads/if_water/food_clean/test/clean/img_20260125_113927_2.jpg"
-    for i in range(len(files)):
+    global new_filepath, diff
+    old_data = []
+    if os.path.exists("delta.json"):
+        with open("delta.json", "r") as f:
+            try:
+                loaded = json.load(f)
+                old_data = loaded if isinstance(loaded, list) else [] # test case for empty file
+            except json.JSONDecodeError:
+                old_data = []
+    for dirpath in os.walk("/Users/jakehopkins/Downloads/if_water/food_clean"):
+        dir = dirpath[0]
+        print(dir)
         files = [f for f in os.listdir(dir) if f.endswith('.jpg')]
-        files = sorted(files)  # sorts the files by timetamp TIMESTAMPS SKIP A FEW NUMBERS
-        new_filepath = os.path.join(dir, files[i])
+        old_filepath = "/Users/jakehopkins/Downloads/if_water/food_clean/test/clean/img_20260125_113927_2.jpg"
+        for i in range(len(files)):
+            files = [f for f in os.listdir(dir) if f.endswith('.jpg')]
+            files = sorted(files)  # sorts the files by timetamp TIMESTAMPS SKIP A FEW NUMBERS
+            new_filepath = os.path.join(dir, files[i])
 
-        print(f"new filepath {new_filepath}")
-        print(f"old filepath {old_filepath}")
-        old = cv.imread(old_filepath)
-        print(f"loop number {i}")
-        new = cv.imread(new_filepath)
-    
-        diff = cv.absdiff(old, new)
-        diff = np.average(diff)
+            
+            old = cv.imread(old_filepath)
+            
+            new = cv.imread(new_filepath)
+        
+            diff = cv.absdiff(old, new)
+            diff = np.average(diff)
 
-        old_filepath = new_filepath
-        # print(f"old{old} new{new}")
-        print(f"difference is {diff} out 255")
-diffs()
+            old_filepath = new_filepath
+            print(f"new filepath {new_filepath}")
+            print(f"old filepath {old_filepath}")
+            print(f"loop number {i}")
+            print(f"difference is {diff} out 255")            
+            old_data.append({
+                "filepath": new_filepath,
+                "diff": float(diff)
+            })
+    with open("delta.json", "w") as f:
+        json.dump(old_data, f, indent=2)
+# diffs()
