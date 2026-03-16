cf053ad351635ffcfd15d9d18929a3218fd4b50b
Learning-howto-Code <jaketorohopkins@icloud.com>
2026-03-16 12:03:48 -0700

    testing on pi

--- test.py
+++ test.py
@@ -4,13 +4,30 @@ import cv2 as cv
 import os
 import time
 import datetime
+from pi5neo import Pi5Neo
+from picamera2 import Picamera2
+
+picam2 = Picamera2()
+config = picam2.create_video_configuration(main={"size": (640, 360)}, buffer_count=4)
+picam2.configure(config)
+picam2.start()
+
+SPI_DEVICE = '/dev/spidev0.0' # Rpi protocol to get the timing right for the GPIOs
+SPI_SPEED_KHZ = 800 #speed of SPI protocol
+
+neo = Pi5Neo(SPI_DEVICE, 30, SPI_SPEED_KHZ)
+
+neo.fill_strip(220, 240, 120)
+neo.update_strip()  # commit/send to LEDs
+time.sleep(0.5)
 
 done = False
 folder_time = time.strftime("%m%d%H:%M")
-base_dir="/Users/jakehopkins/Downloads/if_water/data/sink"
+base_dir="/home/jake/Downloads/if-water-cnn/data/sink"
 out_dir = os.path.join(base_dir, folder_time)
 os.makedirs(out_dir, exist_ok=True)
-img = "/Users/jakehopkins/Downloads/if_water/food_clean_extra_clean/01442_img_20260124_130809_1446.jpg"
+frame = picam2.capture_array()
+img = cv.cvtColor(frame, cv.COLOR_BGR2RGB) #the pi cam takes in BGR
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + f"-{datetime.datetime.now().microsecond // 1000:03d}"
 img_name = f"{timestamp}.jpg"
 cv.imwrite(os.path.join(out_dir, img_name), img)
