ebe1c930d03f3aa68a337bc1c2df1e17aabeeff4
Learning-howto-Code <jaketorohopkins@icloud.com>
2026-05-19 17:30:32 -0700

    reduced compression of images

--- deploy.py
+++ deploy.py
@@ -187,7 +187,7 @@ while True:
              "filepath": imgname
         })
 
-    if food_prediction is not None and food_prediction >.5  and old_food is not True:
+    if food_prediction is not None and food_prediction >.5  and old_food > .5:
         imgname= f"logged_data/{datetime.now().astimezone().strftime('%Y-%m-%d %H-%M-%S %Z')}.jpg"
         save_img = (img[0] * 255).astype(np.uint8)
         cv2.imwrite(imgname, save_img, [cv2.IMWRITE_JPEG_QUALITY, 50]) # saves image with timestamp, can be used for future training data
@@ -199,7 +199,7 @@ while True:
              "food_end": False,
              "filepath": imgname
         })
-    if food_prediction is not None and food_prediction < 0.5 and old_food == True:
+    if food_prediction is not None and food_prediction < 0.5 and old_food < .5:
         imgname= f"logged_data/{datetime.now().astimezone().strftime('%Y-%m-%d %H-%M-%S %Z')}.jpg"
         save_img = (img[0] * 255).astype(np.uint8)
         cv2.imwrite(imgname, save_img, [cv2.IMWRITE_JPEG_QUALITY, 50]) # saves image with timestamp, can be used for future training data
@@ -211,7 +211,7 @@ while True:
              "food_end": True,
              "filepath": imgname
         })
-    if poop_prediction is not None and poop_prediction >.5  and old_poop is not True:
+    if poop_prediction is not None and poop_prediction >.5  and old_poop > .5:
         imgname= f"logged_data/{datetime.now().astimezone().strftime('%Y-%m-%d %H-%M-%S %Z')}.jpg"
         save_img = (img[0] * 255).astype(np.uint8)
         cv2.imwrite(imgname, save_img, [cv2.IMWRITE_JPEG_QUALITY, 50]) # saves image with timestamp, can be used for future training data
@@ -223,7 +223,7 @@ while True:
              "poop_end": False,
              "filepath": imgname
         })
-    if poop_prediction is not None and poop_prediction < 0.5 and old_poop == True:
+    if poop_prediction is not None and poop_prediction < 0.5 and old_poop < .5:
         imgname= f"logged_data/{datetime.now().astimezone().strftime('%Y-%m-%d %H-%M-%S %Z')}.jpg"
         save_img = (img[0] * 255).astype(np.uint8)
         cv2.imwrite(imgname, save_img, [cv2.IMWRITE_JPEG_QUALITY, 50]) # saves image with timestamp, can be used for future training data
