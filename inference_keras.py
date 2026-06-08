5edacdf60dd391a095eda6b8ef5b4be748df691e
Learning-howto-Code <jaketorohopkins@icloud.com>
2026-06-08 10:11:55 -0700

    New if-water model should be working as expected.

--- inference_keras.py
+++ inference_keras.py
@@ -5,10 +5,10 @@ import os
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
-img_dir = '/Users/jakehopkins/Downloads/water test'
+img_dir = '/Users/jakehopkins/Downloads/6-7(no_water)'
 true_label = "water"  # set to "water" or "no water"
 
-keras_model_path = '/Users/jakehopkins/Downloads/if_water/test_if_water_20260605_135419.keras'
+keras_model_path = '/Users/jakehopkins/Downloads/if_water/test_if_water_20260607_184114.keras'
 
 model = tf.keras.models.load_model(keras_model_path, compile=False)
 print("Model loaded:", keras_model_path)
